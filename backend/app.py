25a875e5be7a495f58ca8d87690861854e48c87b
victor-baiao <victorbaiao14@gmail.com>
2025-11-28 15:55:26 -0300

    Preparação para deploy do backend

--- backend/app.py
+++ backend/app.py
@@ -97,4 +97,5 @@ def generate_learning_content():
 if __name__ == '__main__':
     if not os.path.exists('static'):
         os.makedirs('static')
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
